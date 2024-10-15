9b3a7f10a86a677a9ecef954116a3bcc1420aea1
Vishwa500 <143321672+Vishwa500@users.noreply.github.com>
2024-10-15 19:19:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "x61QRazB#itRvnJOFwqvA0X_x7Q9UhgRYbIrgh7Q3OJ5DkrYaepc",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "",
     // ADD Your MongoDB Database URL
